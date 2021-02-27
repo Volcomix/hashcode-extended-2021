0f2a4e1ccfcda1a01efc89e103b0857e02349e27
Volcomix <Volcomix@users.noreply.github.com>
2021-02-27 18:03:01 +0100

    Load dataset text content

--- dataset/index.ts
+++ dataset/index.ts
@@ -1,6 +1,6 @@
-export * as AnExample from './a.txt';
-export * as ByTheOcean from './b.txt';
-export * as Checkmate from './c.txt';
-export * as DailyCommute from './d.txt';
-export * as Etoile from './e.txt';
-export * as ForeverJammed from './f.txt';
+export { default as AnExample } from './a.txt';
+export { default as ByTheOcean } from './b.txt';
+export { default as Checkmate } from './c.txt';
+export { default as DailyCommute } from './d.txt';
+export { default as Etoile } from './e.txt';
+export { default as ForeverJammed } from './f.txt';
--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,73 @@ import React, { useState } from 'react';
 import * as datasetUrls from '../dataset';
 import './App.css';
 
-type SelectedDatasets = { [datasetName: string]: boolean };
+type DatasetName = keyof typeof datasetUrls;
+type SelectedDatasets = { [datasetName in DatasetName]: boolean };
 
-const initialSelectedDatasets = Object.keys(datasetUrls).reduce(
+const datasetNames = Object.keys(datasetUrls) as DatasetName[];
+
+const allDatasetsNotSelected = datasetNames.reduce(
   (acc, datasetName) => Object.assign(acc, { [datasetName]: false }),
+  {} as SelectedDatasets
+);
+const allDatasetsSelected = datasetNames.reduce(
+  (acc, datasetName) => Object.assign(acc, { [datasetName]: true }),
   {} as SelectedDatasets
 );
 
 function App() {
   const [selectedDatasets, setSelectedDatasets] = useState<SelectedDatasets>(
-    initialSelectedDatasets
+    allDatasetsNotSelected
   );
 
-  function updateSelection(datasetName: string) {
+  const isSomeSelected = Object.values(selectedDatasets).some(
+    (isSelected) => isSelected
+  );
+  const areAllSelected = Object.values(selectedDatasets).every(
+    (isSelected) => isSelected
+  );
+
+  function selectOrDeselectAll() {
+    setSelectedDatasets(
+      areAllSelected ? allDatasetsNotSelected : allDatasetsSelected
+    );
+  }
+
+  function updateSelection(datasetName: DatasetName) {
     setSelectedDatasets({
       ...selectedDatasets,
       [datasetName]: !selectedDatasets[datasetName],
     });
   }
 
-  function solve() {
-    console.log(
-      'Solving...',
-      Object.entries(selectedDatasets)
-        .filter(([_datasetName, isSelected]) => isSelected)
-        .map(([datasetName]) => datasetName)
-    );
+  async function solve() {
+    const datasetPromises = datasetNames
+      .filter((datasetName) => selectedDatasets[datasetName])
+      .map(async (datasetName) => {
+        const response = await fetch(datasetUrls[datasetName]);
+        return { datasetName, textContent: await response.text() };
+      });
+
+    for await (const { datasetName, textContent } of datasetPromises) {
+      console.log(datasetName, textContent.length);
+    }
   }
 
   return (
     <div className="App">
-      {Object.keys(datasetUrls).map((datasetName) => (
+      <label>
+        <input
+          type="checkbox"
+          ref={(el) =>
+            el && (el.indeterminate = isSomeSelected && !areAllSelected)
+          }
+          checked={areAllSelected}
+          onChange={selectOrDeselectAll}
+        />
+        All
+      </label>
+      <hr />
+      {datasetNames.map((datasetName) => (
         <label key={datasetName}>
           <input
             type="checkbox"
